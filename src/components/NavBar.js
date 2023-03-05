1c8046ba8026e21ad4e5b1936603e1eab45ab229
Ananas <mrs.anna.noga@gmail.com>
2023-03-05 09:46:33 +0000

    navbar toggle fixed, shows once, toggle icon white

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,7 +21,6 @@ export const NavBar = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav">
                         <span className="navbar-toggler-icon"></span>
                     </Navbar.Toggle>
-                    <Navbar.Toggle />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
