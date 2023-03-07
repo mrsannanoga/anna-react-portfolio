68f7f8606767bee6a165783683b52fce39d272ed
Ananas <mrs.anna.noga@gmail.com>
2023-03-07 19:52:27 +0000

    navbar links works well, just need to get how to remove hash in url later

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { NavBar } from "./components/NavBar";
 import { Home } from "./components/Home";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { Skills } from "./components/Skills";
+
 
 function App() {
   return (
-    <Router> 
-      <div className="App">
-      <NavBar />
-      <Home />
+    <div className="App">
+    <Router>
+          <div>
+            <NavBar />
+            
+            <Routes>
+             
+              <Route path="/" element={<Home/>} />
+              <Route path="/Skills" element={<Skills />} />
+              {/* <Route path="/projects/*" element={<Projects />} />
+              <Route path="contact/*" element={<Contact />} /> */}
+            </Routes>
+          </div>
+          {/* <Footer /> */}
+        </Router>
+        </div>
 
-
-    </div>
-    </Router>
-   
 
 
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,43 +1,46 @@
-import { useState } from "react"
-import { Navbar, Container, Nav } from "react-bootstrap";
 import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+
 import logo from "../assests/img/logo.jpeg";
 import linkedin from "../assests/img/linkedin.svg";
 import github from "../assests/img/github.svg";
+import { Link } from "react-router-dom";
 
 
 export const NavBar = () => {
-    const [activeLink, setActiveLink] = useState('home');
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);
-    }
+    
     return (
-        
-            <Navbar expand="lg">
-                <Container>
-                    <Navbar.Brand href="#home">
-                        <img src={logo} alt="Logo" />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav">
-                        <span className="navbar-toggler-icon"></span>
-                    </Navbar.Toggle>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-                            <Nav.Link href="#about" className={activeLink === 'about' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('about')}>About</Nav.Link>
-                            <Nav.Link href="#resume" className={activeLink === 'resume' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('resume')}>Resume</Nav.Link>
-                            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')} >Projects</Nav.Link>
-                            <Nav.Link href="#contact" className={activeLink === 'contact' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('contact')}>Contact</Nav.Link>
-                        </Nav>
-                        <span className="navbar-text">
-                            <div className="social-icon">
-                                <a href="https://www.linkedin.com/in/mrs-anna-noga/"><img src={linkedin} alt="linkedin icon" /></a>
-                                <a href="https://github.com/mrsannanoga"><img src={github} alt="github icon" /></a>
-                            </div>
-                        </span>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
- 
+
+        <Navbar expand="lg">
+            <Container>
+                <Navbar.Brand>
+                    <Link to= "/">
+                    <img src={logo} alt="Logo" />
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link to="skills"
+                            className= 'nav-link navbar-link navbar-link'>Skills</Link>
+                        <Link to="resume" 
+                            className= 'nav-link navbar-link navbar-link'>Resume</Link>
+                        <Link to="projects" 
+                            className= 'nav-link navbar-link navbar-link'>Projects</Link>
+                        <Link to="contact"
+                            className= 'nav-link navbar-link navbar-link'>Contact</Link>
+                    </Nav>
+                    <span className="navbar-text">
+                        <div className="social-icon">
+                            <a href="https://www.linkedin.com/in/mrs-anna-noga/"><img src={linkedin} alt="linkedin icon" /></a>
+                            <a href="https://github.com/mrsannanoga"><img src={github} alt="github icon" /></a>
+                        </div>
+                    </span>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
     )
 }
