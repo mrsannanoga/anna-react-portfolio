3d5905ae635dbf3d8bcc0547e394dd3ec12a6daf
Ananas <mrs.anna.noga@gmail.com>
2023-03-17 15:44:51 +0000

    browserrouter changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { NavBar } from "./components/NavBar";
 import { Home } from "./components/Home";
 import { Skills } from "./components/Skills";
